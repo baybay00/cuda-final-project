01ca8c2b7e5739af5d6de9eec7fa750082caa79a
Bailey Greer <157333212+baybay00@users.noreply.github.com>
2024-11-10 13:46:31 -0500

    Add files via upload

---
+++ kernel.cu
@@ -0,0 +1,14 @@
+#define BLOCK_SIZE 512
+
+__global__ void bubbleSort(float* in, unsigned size, bool is_even)
+{
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = is_even ? 2 * tx : 2 * tx + 1;
+
+    if(i < size - 1 && in[i] > in[i + 1])
+    {
+        float temp = in[i];
+        in[i] = in[i + 1];
+        in[i + 1] = temp;
+    }
+}
