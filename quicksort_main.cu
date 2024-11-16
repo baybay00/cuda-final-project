f828f6c689db3440975908c291eeb92f1cd88a59
Bailey Greer <157333212+baybay00@users.noreply.github.com>
2024-11-16 15:20:13 -0500

    Add files via upload

--- quicksort_main.cu
+++ quicksort_main.cu
@@ -1,16 +1,21 @@
 #include <stdio.h>
+
 #include "support.h"
 #include "quicksort_kernel.cu"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     Timer timer;
 
     // Initialize host variables ----------------------------------------------
+
     printf("\nSetting up the problem..."); fflush(stdout);
     startTime(&timer);
 
     float *in_h;
     float *in_d;
+    int* stack_h;
+    int* stack_d;
     unsigned in_elements;
     cudaError_t cuda_ret;
     dim3 dim_grid, dim_block;
@@ -29,32 +34,36 @@ int main(int argc, char* argv[]) {
     }
     initVector(&in_h, in_elements);
 
+    stack_h = (int*)malloc(in_elements * sizeof(int));
+
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
     printf("    Input size = %u\n", in_elements);
 
     // Allocate device variables ----------------------------------------------
+
     printf("Allocating device variables..."); fflush(stdout);
     startTime(&timer);
 
     cuda_ret = cudaMalloc((void**)&in_d, in_elements * sizeof(float));
     if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory: in");
 
-    int *d_stack, *d_top;
-    cuda_ret = cudaMalloc((void**)&d_stack, 2 * in_elements * sizeof(int));  // Stack size can be up to 2*in_elements
-    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory for stack");
-
-    cuda_ret = cudaMalloc((void**)&d_top, sizeof(int));
-    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory for top");
+    cuda_ret = cudaMalloc((void**)&stack_d, in_elements * sizeof(int));
+    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory: stack");
 
     cudaDeviceSynchronize();
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 
     // Copy host variables to device ------------------------------------------
+
     printf("Copying data from host to device..."); fflush(stdout);
     startTime(&timer);
 
-    cuda_ret = cudaMemcpy(in_d, in_h, in_elements * sizeof(float), cudaMemcpyHostToDevice);
+    cuda_ret = cudaMemcpy(in_d, in_h, in_elements * sizeof(float),
+        cudaMemcpyHostToDevice);
     if(cuda_ret != cudaSuccess) FATAL("Unable to copy memory to the device: in");
+
+    cuda_ret = cudaMemcpy(stack_d, stack_h, in_elements*sizeof(int), cudaMemcpyHostToDevice);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to copy memory to the device: stack");
 
     cudaDeviceSynchronize();
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
@@ -73,9 +82,9 @@ int main(int argc, char* argv[]) {
     startTime(&timer);
 
     dim_block.x = BLOCK_SIZE;
-    dim_grid.x = (in_elements + BLOCK_SIZE - 1) / BLOCK_SIZE;  // Adjust grid size
+    dim_grid.x = (in_elements + 2 * BLOCK_SIZE -1)/(2 * BLOCK_SIZE);
 
-    quicksort_kernel<<<dim_grid, dim_block>>>(in_d, d_stack, d_top, in_elements);
+    quicksort_kernel<<<dim_grid, dim_block>>>(in_d, 0, in_elements - 1, stack_d);
     cuda_ret = cudaDeviceSynchronize();
     if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
     
@@ -83,6 +92,7 @@ int main(int argc, char* argv[]) {
     printf("Device quick sort time: %f s\n", elapsedTime(timer));
 
     // Verify correctness -----------------------------------------------------
+
     printf("Verifying results...\n"); fflush(stdout);
 
     float* sorted_h = (float*)malloc((in_elements) * sizeof(float));
@@ -102,13 +112,14 @@ int main(int argc, char* argv[]) {
     } else {
         printf("Sorting failed\n\n");
     }
-
     // Free memory ------------------------------------------------------------
+
     cudaFree(in_d);
-    cudaFree(d_stack);
-    cudaFree(d_top);
+    cudaFree(stack_d);
+    free(stack_h);
     free(in_h);
     free(sorted_h);
 
     return 0;
 }
+
--- shellsort_kernel.cu
+++ shellsort_kernel.cu
@@ -1,31 +1,22 @@
-#define BLOCK_SIZE 128
+#define BLOCK_SIZE 1024
 
 __global__ void shellsort_kernel(float* in, unsigned size, unsigned gap)
 {
-    extern __shared__ float sdata[];
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (tid < size) {
-        sdata[threadIdx.x] = in[tid];
-        __syncthreads();
-        for (int i = tid; i < size - 1; i += blockDim.x * gridDim.x)
+    for(int i = tid; i < size - 1; i += blockDim.x * gridDim.x)
+    {
+        for(int j = i + gap; j < size; j += gap)
         {
-            for (int j = i + gap; j < size; j += gap)
-            {
-                float temp = sdata[j];
-                int k = j;
+            float temp = in[j];
+            int k = j;
 
-                while (k >= gap && sdata[k - gap] > temp)
-                {
-                    sdata[k] = sdata[k - gap];
-                    k -= gap;
-                }
-                sdata[k] = temp;
+            while(k >= gap && in[k - gap] > temp)
+            {
+                in[k] = in[k - gap];
+                k -= gap;
             }
-        }
-        __syncthreads();
-        if (tid < size) {
-            in[tid] = sdata[threadIdx.x];
+            in[k] = temp;
         }
     }
 }
